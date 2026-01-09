e50d1c7bf100790b71dafc991141041b0199b08d
ProWhale123 <arpadbretz@gmail.com>
2026-01-09 13:35:04 +0100

    Fix: Add missing useState import and 'use client' directive in AddTradeForm

--- components/AddTradeForm.tsx
+++ components/AddTradeForm.tsx
@@ -1,4 +1,6 @@
-import { useEffect } from 'react';
+'use client';
+
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
