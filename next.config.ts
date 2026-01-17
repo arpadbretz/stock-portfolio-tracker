cf4d2a537aea46c181b2a1d5d957a19b7fd48e53
ProWhale123 <arpadbretz@gmail.com>
2026-01-17 19:42:52 +0100

    feat: integrate PostHog and Sentry for analytics and error tracking

--- next.config.ts
+++ next.config.ts
@@ -14,18 +14,9 @@ export default withSentryConfig(nextConfig, {
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
   // Routes HTTP calls to Sentry through a Next.js rewrite to circumvent ad-blockers (slows down your site)
   tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
 });
